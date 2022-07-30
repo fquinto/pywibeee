82ad60299a5a02abc692dd8637324304974eb7f9
Fran Quinto <1702904+fquinto@users.noreply.github.com>
2022-07-30 14:26:57 +0200

    Improvements firmware downloader

--- firmware/download_check.py
+++ firmware/download_check.py
@@ -1,39 +1,52 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# AUTHOR    | DATE       | VERSION | COMMENTS
+# F.Quinto  | 2022-07-30 | 0.0.0   | First version
+
+"""Firmware downloader.
+
+sudo -H pip3 install httpx
+"""
+
+import httpx
 import requests
 import urllib.request
 
 download = True
 continueWhiling = True
 v1 = 3
-v2 = 6
-v3 = 0
-
+v2 = 4
+v3 = 600
+# v1 = 4
+# v2 = 4
+# v3 = 123
+models = [
+    'WBB',
+    'WB3'
+]
+print('Starting!')
 while continueWhiling:
-    
-    fname = f'FOTAFile_V{v1}.{v2}.{v3}_WBB.bin'
-    url = f'https://app.mirubee.com/api/fw/wbb/{fname}'
-    print(f'Trying {url}...')
-    if download:
-        r = requests.get(url)
-        if r.status_code == 200:
-            print('Found!', end='')
-            # print(r.status_code)
-            # print(r.headers['content-type'])
-            # print(r.encoding)
-            with open(f'{fname}', 'wb') as f:
-                f.write(r.content)
-    v3 += 1
-    if v3 > 999:
-        v2 += 1
-        v3 = 0
-    if v2 > 99:
-        v1 += 1
-        v2 = 0
-    if v1 > 5:
-        continueWhiling = False
-
-# urllib.request.urlretrieve(url, fname)
-# r = requests.get(url, stream=True)
-# print(r.headers)
+    for model in models:
+        fname = f'FOTAFile_V{v1}.{v2}.{v3}_{model}.bin'
+        model_lower = model.lower()
+        url = f'https://app.mirubee.com/api/fw/{model_lower}/{fname}'
+        # print(f'Trying {url}...')
+        print(f'Checking version {v1}.{v2}.{v3} and model {model}...', end='', flush=True)
+        if download:
+            client = httpx.Client()
+            r = client.get(url)
+            if r.status_code == 200:
+                print(f'\n\n{url} Found!\n')
+                r2 = requests.get(url)
+                with open(f'{fname}', 'wb') as f:
+                    f.write(r2.content)
+        v3 += 1
+        if v3 > 999:
+            v2 += 1
+            v3 = 0
+        if v2 > 99:
+            v1 += 1
+            v2 = 0
+        if v1 > 5:
+            continueWhiling = False

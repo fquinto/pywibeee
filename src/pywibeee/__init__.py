fb6956fe06a0c0a5e1327acf5967fb587c272ef4
fquinto <1702904+fquinto@users.noreply.github.com>
2026-04-17 13:15:13 +0200

    Refactor to use src layout and separate client library
    - Create src/pywibeee/ with client.py (API client extracted from HA)
    - Update to version 0.1.2
    - Add aiohttp as dependency for HA integration
    - Move main.py to cli.py for CLI functionality
    - Update pyproject.toml for src layout

---
+++ src/pywibeee/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 @fquinto. All rights reserved.
+
+__version__ = "0.1.2"
+
+from pywibeee.client import WibeeeAPI, WibeeeDeviceInfo
+
+__all__ = ["WibeeeAPI", "WibeeeDeviceInfo", "__version__"]

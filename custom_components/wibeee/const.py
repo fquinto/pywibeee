590031f4921fbb493e5782bbb035250efc53bf84
fquinto <1702904+fquinto@users.noreply.github.com>
2026-04-17 13:26:52 +0200

    Use UnitOfReactiveEnergy constants instead of raw 'varh' strings

--- custom_components/wibeee/const.py
+++ custom_components/wibeee/const.py
@@ -10,6 +10,16 @@ from homeassistant.components.sensor import (
     SensorEntityDescription,
     SensorStateClass,
 )
+from homeassistant.const import (
+    UnitOfApparentPower,
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
+    UnitOfEnergy,
+    UnitOfFrequency,
+    UnitOfPower,
+    UnitOfReactiveEnergy,
+    UnitOfReactivePower,
+)
 
 DOMAIN = "wibeee"
 
@@ -148,14 +158,14 @@ SENSOR_TYPES: dict[str, WibeeeSensorEntityDescription] = {
     "energia_reactiva_ind": WibeeeSensorEntityDescription(
         key="energia_reactiva_ind",
         translation_key="inductive_reactive_energy",
-        native_unit_of_measurement="varh",
+        native_unit_of_measurement=UnitOfReactiveEnergy.VOLT_AMPERE_REACTIVE_HOUR,
         device_class=None,
         state_class=SensorStateClass.TOTAL_INCREASING,
     ),
     "energia_reactiva_cap": WibeeeSensorEntityDescription(
         key="energia_reactiva_cap",
         translation_key="capacitive_reactive_energy",
-        native_unit_of_measurement="varh",
+        native_unit_of_measurement=UnitOfReactiveEnergy.VOLT_AMPERE_REACTIVE_HOUR,
         device_class=None,
         state_class=SensorStateClass.TOTAL_INCREASING,
         entity_registry_enabled_default=False,
--- pywibeee/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2019 @fquinto. All rights reserved.
-# license that can be found in the LICENSE file.
-
-name = "pywibeee"
-__version__ = "0.1.1"
